d5fdf46a0698af7ee7fe2598da8747b2b9a60074
hasanhamidi <hamidi3031@gmail.com>
2022-06-06 16:24:39 +0430

    1

--- trainer_me.py
+++ trainer_me.py
@@ -106,6 +106,7 @@ if __name__ == '__main__':
                         loss_function = F.cross_entropy,
                         scheduler = None,
                         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
+    trainer.train_one_epoch(0)
 
 
 
