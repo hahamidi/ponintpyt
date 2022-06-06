070089daa2452898b97753ab37e0a30af51e38e9
hasanhamidi <hamidi3031@gmail.com>
2022-06-06 18:24:10 +0430

    1

--- trainer_me.py
+++ trainer_me.py
@@ -112,10 +112,40 @@ class Trainer():
                     epoch_val_acc.append(accuracy)
                     batch_iter.set_description(self.red('val loss: %f, val accuracy: %f' % (np.mean(epoch_val_loss),
                                                                             np.mean(epoch_val_acc))))
+    def evaluate_miou(self):
+        shape_ious = []
+        batch_iter = tqdm(enumerate(self.val_data_loader), 'Miou on val', total=len(self.val_data_loader),
+                           position=0)
+        for i,data in batch_iter:
+            points, target = data
+            points = points.transpose(2, 1)
+            points, target = points.cuda(), target.cuda()
+            classifier = self.model.eval()
+            pred, _= classifier(points)
+            pred_choice = pred.data.max(2)[1]
+
+            pred_np = pred_choice.cpu().data.numpy()
+            target_np = target.cpu().data.numpy() - 1
+
+            for shape_idx in range(target_np.shape[0]):
+                parts = range(self.num_classes)#np.unique(target_np[shape_idx])
+                part_ious = []
+                for part in parts:
+                    I = np.sum(np.logical_and(pred_np[shape_idx] == part, target_np[shape_idx] == part))
+                    U = np.sum(np.logical_or(pred_np[shape_idx] == part, target_np[shape_idx] == part))
+                    if U == 0:
+                        iou = 1 #If the union of groundtruth and prediction points is empty, then count part IoU as 1
+                    else:
+                        iou = I / float(U)
+                    part_ious.append(iou)
+                shape_ious.append(np.mean(part_ious))
+        print("Mean IOU: ", np.mean(shape_ious))
+        return np.mean(shape_ious)
 
 
     def train(self):
         for epoch in range(self.epochs):
+            self.evaluate_miou(epoch)
             self.train_one_epoch(epoch)
             self.val_one_epoch(epoch)
             # self.scheduler.step()
