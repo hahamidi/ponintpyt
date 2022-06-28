0f012108cda99e92ea8bf26163847988f2f140db
hasanhamidi <hamidi3031@gmail.com>
2022-06-28 22:55:31 +0430

    add DGCNN and poinNetPlus

---
+++ model/pointnetPlus.py
@@ -0,0 +1,132 @@
+
+from turtle import forward
+import torch
+import torch.nn as nn
+from pointnet2_ops_lib.pointnet2_ops.pointnet2_modules import PointnetFPModule, PointnetSAModule
+
+print(PointnetFPModule)
+
+class PointNet2SemSegSSG(nn.Module):
+
+
+
+    def __init__(self, hparams,num_classes):
+        super().__init__()
+        self.hparams = hparams
+        self.num_classes = num_classes
+        self.flag = 1
+            # self.metric_moiu = MeanIoU(num_classes=13)
+        self._build_model()
+
+    def _build_model(self):
+        self.SA_modules = nn.ModuleList()
+        self.SA_modules.append(
+            PointnetSAModule(
+                npoint=1024,
+                radius=0.1,
+                nsample=32,
+                mlp=[6, 32, 32, 64],
+                use_xyz=self.hparams["model.use_xyz"],
+            )
+        )
+        self.SA_modules.append(
+            PointnetSAModule(
+                npoint=256,
+                radius=0.2,
+                nsample=32,
+                mlp=[64, 64, 64, 128],
+                use_xyz=self.hparams["model.use_xyz"],
+            )
+        )
+        self.SA_modules.append(
+            PointnetSAModule(
+                npoint=64,
+                radius=0.4,
+                nsample=32,
+                mlp=[128, 128, 128, 256],
+                use_xyz=self.hparams["model.use_xyz"],
+            )
+        )
+        self.SA_modules.append(
+            PointnetSAModule(
+                npoint=16,
+                radius=0.8,
+                nsample=32,
+                mlp=[256, 256, 256, 512],
+                use_xyz=self.hparams["model.use_xyz"],
+            )
+        )
+
+        self.FP_modules = nn.ModuleList()
+        self.FP_modules.append(PointnetFPModule(mlp=[128 + 6, 128, 128, 128]))
+        self.FP_modules.append(PointnetFPModule(mlp=[256 + 64, 256, 128]))
+        self.FP_modules.append(PointnetFPModule(mlp=[256 + 128, 256, 256]))
+        self.FP_modules.append(PointnetFPModule(mlp=[512 + 256, 256, 256]))
+
+        self.fc_lyaer = nn.Sequential(
+            nn.Conv1d(128, 128, kernel_size=1, bias=False),
+            nn.BatchNorm1d(128),
+            nn.ReLU(True),
+            nn.Dropout(0.5),
+            nn.Conv1d(128, self.num_classes, kernel_size=1),
+            
+
+             )
+
+    def _break_up_pc(self, pc):
+        xyz = pc[..., 0:3].contiguous()
+        features = pc[..., 3:].transpose(1, 2).contiguous() if pc.size(-1) > 3 else None
+
+        return xyz, features
+        
+    def forward(self, pointcloud):
+        r"""
+            Forward pass of the network
+
+            Parameters
+            ----------
+            pointcloud: Variable(torch.cuda.FloatTensor)
+                (B, N, 3 + input_channels) tensor
+                Point cloud to run predicts on
+                Each point in the point-cloud MUST
+                be formated as (x, y, z, features...)
+        """
+        xyz, features = self._break_up_pc(pointcloud)
+        l_xyz, l_features = [xyz], [features]
+        for i in range(len(self.SA_modules)):
+            li_xyz, li_features = self.SA_modules[i](l_xyz[i], l_features[i])
+            l_xyz.append(li_xyz)
+            l_features.append(li_features)
+        for i in range(-1, -(len(self.FP_modules) + 1), -1):
+            l_features[i - 1] = self.FP_modules[i](
+                l_xyz[i - 1], l_xyz[i], l_features[i - 1], l_features[i]
+            )
+
+        classification = self.fc_lyaer(l_features[0])
+
+      
+
+        return classification
+
+
+
+
+class model_with_head(torch.nn.Module):
+    def __init__(self,model_bkb):
+        super().__init__()
+        self.Back_bone_model = model_bkb
+
+
+        self.fc_head = nn.Sequential(
+
+            nn.BatchNorm1d(128),
+            nn.ReLU(True),
+            nn.Dropout(0.5),
+            nn.Conv1d(128, 13, kernel_size=1),
+        )
+
+    def forward(self,pointcloud):
+        bcb_out = self.Back_bone_model(pointcloud)
+        out = self.fc_head(bcb_out)
+        return out
+
---
+++ train_DGCNN.py
@@ -0,0 +1,78 @@
+
+import argparse
+import torch
+import torch.optim as optim
+import torch.nn.functional as F
+from datasets import ShapeNetDataset, PointMNISTDataset
+from trainer_me import Trainer
+from model.DGCNN import DGCNN_partseg
+
+
+
+
+    
+
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dataset', type=str, choices=['shapenet', 'mnist'], help='dataset to train on')
+    parser.add_argument('dataset_folder', type=str, help='path to the dataset folder')
+    parser.add_argument('task', type=str, choices=['classification', 'segmentation'], help='type of task')
+    parser.add_argument('output_folder', type=str, help='output folder')
+    parser.add_argument('--num_points', type=int, default=2500, help='number of points per cloud')
+    parser.add_argument('--batch_size', type=int, default=32, help='batch size')
+    parser.add_argument('--epochs', type=int, default=20, help='number of epochs')
+    parser.add_argument('--learning_rate', type=float, default=0.001, help='learning rate')
+    parser.add_argument('--number_of_workers', type=int, default=1, help='number of workers for the dataloader')
+    parser.add_argument('--model_checkpoint', type=str, default='', help='model checkpoint path')
+    parser.add_argument('--num_points', type=int, default=2048,help='num of points to use')
+    parser.add_argument('--dropout', type=float, default=0.5,help='dropout rate')
+    parser.add_argument('--emb_dims', type=int, default=1024, metavar='N',help='Dimension of embeddings')
+    parser.add_argument('--k', type=int, default=40, metavar='N',help='Num of nearest neighbors to use')
+
+    args = parser.parse_args()
+
+
+    DATASETS = {
+                'shapenet': ShapeNetDataset,
+                'mnist': PointMNISTDataset
+            }
+    train_dataset = DATASETS[args.dataset](args.dataset_folder,
+                                      task=args.task,
+                                      number_of_points=args.num_points)
+    train_dataloader = torch.utils.data.DataLoader(train_dataset,
+                                                        batch_size=args.batch_size,
+                                                        shuffle=True,
+                                                        num_workers=args.number_of_workers)
+    test_dataset = DATASETS[args.dataset](args.dataset_folder,
+                                            task=args.task,
+                                            train=False,
+                                            number_of_points=args.num_points)
+    test_dataloader = torch.utils.data.DataLoader(test_dataset,
+                                                        batch_size=args.batch_size,
+                                                        shuffle=True,
+                                                        num_workers=args.number_of_workers)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = DGCNN_partseg(args, train_dataset.NUM_SEGMENTATION_CLASSES).to(device)
+    optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
+    
+    
+    print(device)
+    
+    trainer = Trainer(model = model,
+                        train_data_loader = train_dataloader, 
+                        val_data_loader = test_dataloader, 
+                        optimizer = optimizer,
+                        epochs=args.epochs,
+                        number_of_classes = train_dataset.NUM_SEGMENTATION_CLASSES,
+                        loss_function = F.cross_entropy,
+                        scheduler = None,
+                        device =device)
+
+    print(train_dataset.NUM_SEGMENTATION_CLASSES)
+    trainer.train()
+
+
+
+
+
