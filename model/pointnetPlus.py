59243cf0455d12731219671871a411311ccfeb3d
hasanhamidi <hamidi3031@gmail.com>
2022-06-30 00:32:06 +0430

    1

--- model/pointnetPlus.py
+++ model/pointnetPlus.py
@@ -92,8 +92,7 @@ class PointNet2SemSegSSG(nn.Module):
                 be formated as (x, y, z, features...)
         """
         xyz, features = self._break_up_pc(pointcloud)
-        print(xyz,features)
-        l_xyz, l_features = [xyz], [features]
+        l_xyz, l_features = [xyz], [xyz]
         for i in range(len(self.SA_modules)):
             li_xyz, li_features = self.SA_modules[i](l_xyz[i], l_features[i])
             l_xyz.append(li_xyz)
